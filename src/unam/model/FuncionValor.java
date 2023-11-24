aafe7d6bfe9de488c50754a737dba1b076fc1c69
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2023-11-23 23:22:34 -0600

    added model FuncionValor

---
+++ src/unam/model/FuncionValor.java
@@ -0,0 +1,37 @@
+package unam.model;
+
+public class FuncionValor {
+    String nombre;
+    Double peso;
+    String ruta;
+
+    public FuncionValor(String nombre, Double peso, String ruta) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.ruta = ruta;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Double peso) {
+        this.peso = peso;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+}
