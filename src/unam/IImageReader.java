41720d8ed68fc7663aabbee033d4c05f263a3145
temobgallardo <62340961+temobgallardo@users.noreply.github.com>
2023-11-23 23:21:15 -0600

    fixed file extension and commented for now

---
+++ src/unam/IImageReader.java
@@ -0,0 +1,6 @@
+package unam;
+
+// TODO: implement this
+// public interface IImageReader {
+// public BufferedImage ReadImage(String fileSource);
+// }
---
+++ src/unam/ImageReader.java
@@ -0,0 +1,20 @@
+package unam;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+// TODO: implement this
+// public class ImageReader implements IImageReader {
+// public BufferedImage ReadImage(String fileSource) {
+// BufferedImage img = null;
+// try {
+// img = ImageIO.read(new File(fileSource));
+// } catch (IOException e) {
+// e.printStackTrace();
+// }
+
+// return img;
+// }
+// }
